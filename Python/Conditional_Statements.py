8092efb596a40a7e2798cc6066aa879a5a824435
chandru-md <chandrumd2071@gmail.com>
2026-03-05 00:46:08 +0530

    python and machine learning and conditional statements

---
+++ Python/Conditional_Statements.py
@@ -0,0 +1,139 @@
+"""score = 95
+submitted_project = True
+if score >= 90:
+    if submitted_project:
+        print("A+")
+    else:
+        print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")"""
+
+"""score = 50
+submitted_project = False
+if score >= 90:
+    if submitted_project:
+        print("A+")
+    else:
+        print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+elif score >= 50:
+    print("E")
+else:
+    print("F")"""
+
+'''score = 88
+submitted_project = True
+if score >= 90 and submitted_project:
+        print("A+")
+elif score >= 90:
+        print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")'''
+
+score = 50
+submitted_project = True
+
+if score >=90:
+    print("Highest Score")
+else:
+    print("Lowest Score")
+
+if submitted_project:
+    print("Project Submitted")
+else:
+    print("project not submitted.")
+
+
+# inline if statement
+score = 50
+grade = "A" if score >=90 else "B" if score>=80 else 'F'
+print(grade)
+
+# simple logic = inline if statement
+# complex logic = classix if statement
+
+country = "India"
+if country == "India":
+    print("IN")
+elif country == "USA":
+    print("USA")
+elif country == "United Kingdom":
+    print("UK")
+elif country == "Germany":
+    print("DE")
+else:
+    print("Unknown Country")
+
+match country:
+    case "India" | "IND":
+        print("IN")
+    case "USA" | "United States":
+        print("US")
+    case "United Kingdom" | "UK":
+        print("UK")
+    case "Germany" | "DE":
+        print("DE")
+    case _:
+        print("Unknown Country")
+
+
+# task-email
+email = input("Enter your email: ").strip().lower()
+if email == "":
+    print("Invalid email")
+elif not ("@" in email and "." in email):
+    print("Email must contain @ and .")
+elif email.count('@')!=1:
+    print("Email must contain only one @")
+elif not email.endswith((".com",".org",".net")):
+    print("Email must end with .org, .net or .com")
+elif len(email) > 254:
+    print("Email is too long")
+elif not (email[0].isalnum() and email[-1].isalnum()):
+    print("Email must start and end with an alphanumeric character")
+else:
+    print("Email is valid")
+
+
+# task-email
+email = input("Enter your email: ").strip().lower()
+valid = True
+if email == "":
+    print("Invalid email")
+    valid = False
+if not ("@" in email and "." in email):
+    print("Email must contain @ and .")
+    valid = False
+if email.count('@')!=1:
+    print("Email must contain only one @")
+    valid = False
+if not email.endswith((".com",".org",".net")):
+    print("Email must end with .org, .net or .com")
+    valid = False
+if len(email) > 254:
+    print("Email is too long")
+    valid = False
+if not (email[0].isalnum() and email[-1].isalnum()):
+    print("Email must start and end with an alphanumeric character")
+    valid = False
+if valid:
+    print("Email is valid")
+
+
