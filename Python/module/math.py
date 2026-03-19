9f1c5e3996d305024e2414a1ffce0b3331a7a09e
chandru-md <chandrumd2071@gmail.com>
2026-03-19 23:53:30 +0530

    Python-import-modules-packages

--- Python/module/math.py
+++ Python/module/math.py
@@ -1,2 +1,14 @@
 def add(a,b):
     return a+b
+
+def sub(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def division(a/b):
+    if b!=0:
+        return a/b:
+    else:
+        print("denominator must be a number.")
